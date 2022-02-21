6c71b73e8e222e16a3bcb6ac0bf3bf697fd9e365
Manoel M. Neto <manoel.neto@avantsoft.com.br>
2022-02-21 15:24:02 -0300

    Update mongoid version; add configuration to not raise error when document is not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,6 @@ class ApplicationController < ActionController::API
     authenticate_or_request_with_http_token do |token, _options|
       # Compare the tokens in a time-constant manner, to mitigate
       # timing attacks.
-      p token
-      p ENV['ACCESS_TOKEN']
       ActiveSupport::SecurityUtils.secure_compare(token, ENV['ACCESS_TOKEN'])
     end
   end
